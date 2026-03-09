6d7b42d5a6a07d32dce7dd2ef97d5bfd49c8dbf3
SilviaRamirezG <sramirezgarcia5@gmail.com>
2026-03-09 12:20:57 +0100

    Añadido el la pagina de inicio de sesion

--- recapi_project/urls.py
+++ recapi_project/urls.py
@@ -15,8 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include 
+from rest_framework.routers import DefaultRouter
+from recetas.views import RecetaViewSet 
+
+router = DefaultRouter()
+router.register(r'recetas', RecetaViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    # Ruta para tus recetas (http://127.0.0.1:8000/api/recetas/)
+    path('api/', include(router.urls)),
+    
+    # Para loguear (http://127.0.0.1:8000/api-auth/login/)
+    path('api-auth/', include('rest_framework.urls')),
 ]
